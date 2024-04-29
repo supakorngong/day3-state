633c3532c7e89136f7da669fc9d0510c0b8a7337
supakorngong <supurkorn@hotmail.com>
2024-04-29 16:09:11 +0700

    input and select

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-// import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 
-
+// <input/> พิมได้ เเต่ react จะไม่รู้สิ่งที่ user พิม (Uncontrolled Component)
+// <input value="codecamp"/> : controlled Component (Hardcode)
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// function App(){
+//   const [data,setDate] = React.useState("");
+//   // const data = "codecamp"
+//   const handleChange = (event) =>{
+//     setDate(event.target.value); //react จะรู้สิ่งที่ user พิม เรียก control component เนื่องจากมีการ set state
+//     console.log(event.target.value)
+//   }
+//   return <input value={data} onChange={handleChange}/>;
+// }
+
 function App(){
+  const [selected,setselected] = React.useState("3")
+  const handleSelect = (event) =>{
+    // console.log(event.target.value)
+    setselected(event.target.value)
+  }
   return(
-    <div>
-      hello react app
-    </div>
+    <select onChange={handleSelect} value={selected}>
+      <option >option-1</option>
+      <option value="2">option-2</option>
+      <option value="3">option-3</option>
+    </select>
   )
 }
+
 root.render(
     <App />
 );
